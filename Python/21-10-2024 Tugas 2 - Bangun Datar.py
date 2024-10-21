10b47d3445a85c3bf77212bf2625358a41fd9473
Muhammad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-10-21 22:06:02 +0700

    ⛏️ Unit Break 2

--- Python/21-10-2024 Tugas 2 - Bangun Datar.py
+++ Python/21-10-2024 Tugas 2 - Bangun Datar.py
@@ -1,3 +1,4 @@
+import math
 # Buatlah program untuk Menghitung luas dan volume bangun (datar / ruang) dengan tampilan:
 
 # ==================================================
@@ -20,26 +21,66 @@
 # *** PILIHAN BANGUN : ............INPUT ***
 # ==================================================
 
-# Referensi Rumus
+# Referensi Rumus - Bangun Datar
 
-# Persegi:
-# • Luas: sisi x sisi
-# • Keliling: 4 x sisi
+# Persegi:                                  # Jajar Genjang:
+# • Luas: sisi x sisi                       # • Luas: alas x tinggi
+# • Keliling: 4 x sisi                      # • Keliling: 2 x (sisi atas + sisi samping)
 
-# Persegi Panjang
-# • Luas: panjang x lebar
-# • Keliling: 2 x (panjang + lebar)
+# Persegi Panjang:                          # Belah Ketupat:
+# • Luas: panjang x lebar                   # • Luas: 1/2 x diagonal1 x diagonal2
+# • Keliling: 2 x (panjang + lebar)         # • Keliling: 4 x sisi
 
-# List untuk memisahkan
+# Segitiga:                                 # Layang-Layang:
+# • Luas: 1/2 x alas x tinggi               # • Luas: 1/2 x diagonal1 x diagonal2
+# • Keliling: sisi1 + sisi2 + sisi3         # • Keliling: sisi1 + sisi2 + sisi3 + sisi4
+
+# Lingkaran:                                # Trapesium:
+# • Luas: π x r² (π = 22/7 atau 3,14)       # • Luas: 1/2 x (alas atas + alas bawah) x tinggi
+# • Keliling: 2 x π x r                     # • Keliling: sisi atas + sisi samping1 + sisi bawah + sisi samping2
+
+# Referensi Rumus - Bangun Ruang
+
+# Kubus:
+# • Luas permukaan: 6 x sisi²
+# • Volume: sisi³
+
+# Balok:
+# • Luas permukaan: 2 x ((panjang x lebar) + (panjang x tinggi) + (lebar x tinggi))
+# • Volume: panjang x lebar x tinggi
+
+# Prisma:
+# • Luas permukaan: (2 x luas alas) + (keliling alas x tinggi)
+# • Volume: luas alas x tinggi
+
+# Limas:
+# • Luas permukaan: luas alas + (1/2 x keliling alas x tinggi sisi tegak)
+# • Volume: 1/3 x luas alas x tinggi
+
+# Tabung:
+# • Luas permukaan: 2 x π x r x (r + tinggi)
+# • Volume: π x r² x tinggi
+
+# Kerucut:
+# • Luas permukaan: π x r x (r + tinggi)
+# • Volume: 1/3 x π x r² x tinggi
+
+# Bola:
+# • Luas permukaan: 4 x π x r²
+# • Volume: 4/3 x π x r³
+
+
+# List untuk memisahkan.
 bangun = ["DATAR", "RUANG"]
 bangun_datar = [
     "PERSEGI", "PERSEGI PANJANG", "SEGITIGA", "LINGKARAN",
-    "TRAPESIUM", "JAJAR GENJANG", "LINGKARAN", "BELAH KETUPAT"
+    "JAJAR GENJANG", "BELAH KETUPAT", "LAYANG-LAYANG", "TRAPESIUM"
     ]
 bangun_ruang = [
     "KUBUS", "BALOK", "PRISMA", "LIMAS",
     "TABUNG", "KERUCUT", "BOLA"
 ]
+rumus = ["LUAS", "KELILING", "VOLUME", "LUAS PERMUKAAN"]
 
 #   ____                                  _____        _               #
 #  |  _ \                                |  __ \      | |              #
@@ -50,15 +91,26 @@ bangun_ruang = [
 #                      __/ |                                           #
 #                     |___/                                            #
 
-# Input - Bangun Datar / Bangun Ruang
+print("""
+______  _  _  _  _      ______                                    
+| ___ \(_)| |(_)| |     | ___ \                                   
+| |_/ / _ | | _ | |__   | |_/ /  __ _  _ __    __ _  _   _  _ __  
+|  __/ | || || || '_ \  | ___ \ / _` || '_ \  / _` || | | || '_ \ 
+| |    | || || || | | | | |_/ /| (_| || | | || (_| || |_| || | | |
+\_|    |_||_||_||_| |_| \____/  \__,_||_| |_| \__, | \__,_||_| |_|
+                                               __/ |              
+                                              |___/               
+""")
+
+# Input - Bangun Datar / Bangun Ruang.
 while True:
-        input_bangun = str(input("Masukkan pilihan bangun - datar / ruang: ")).upper()
+        input_bangun = str(input("Masukkan pilihan bangun - Datar / Ruang: ")).upper()
         if input_bangun in bangun:
             break
         else:
             print("📢 Pilihan tidak ada, silakan pilih kembali!")
 
-# Input - Bangun Datar
+# Input - Bangun Datar.
 while True:
     if input_bangun == "DATAR":
         print(r"""
@@ -78,9 +130,154 @@ while True:
    3. Segitiga            7. Lingkaran
    4. Lingkaran           8. Belah Ketupat
 """)
-        input_bangun_datar = str(input("Masukkan Pilihan Bangun Datar: "))
+        input_bangun_datar = str(input("Masukkan Pilihan Bangun Datar: ")).upper()
         if input_bangun_datar in bangun_datar:
             break
         else:
             print("📢 Bangun datar tidak ada yang terpilih, silakan pilih kembali!")
             continue
+
+if input_bangun_datar == "PERSEGI":
+    print(r"""
+  ___                           _ 
+ | _ \ ___  _ _  ___ ___  __ _ (_)
+ |  _// -_)| '_|(_-</ -_)/ _` || |
+ |_|  \___||_|  /__/\___|\__, ||_|
+                         |___/    
+""")
+
+# Input - Sisi Persegi.
+while input_bangun_datar == "PERSEGI":
+    try:
+        sisi_persegi = float(input("Masukkan sisi persegi: "))
+
+        print("\n======= Luas Persegi =======")
+        print(f"Sisi Persegi adalah: {sisi_persegi:,.2f} cm")
+        print(f"Rumus Luas Persegi adalah: {sisi_persegi:,.2f} cm x {sisi_persegi:.2f} cm")
+        print(f"Luas Persegi adalah: {sisi_persegi * sisi_persegi:,.2f} cm²")
+
+        print("\n======= Keliling Persegi =======")
+        print(f"Sisi Persegi adalah: {sisi_persegi:,.2f} cm")
+        print(f"Rumus Keliling Persegi adalah: 4 x {sisi_persegi:,.2f} cm")
+        print(f"Keliling Persegi adalah: {4 * sisi_persegi:,.2f} cm")
+
+    except ValueError:
+        print("📢 Tolong Masukkan Angka!")
+        continue
+    else:
+        break
+
+if input_bangun_datar == "PERSEGI PANJANG":
+    print(r"""
+  ___                           _   ___               _                   
+ | _ \ ___  _ _  ___ ___  __ _ (_) | _ \ __ _  _ _   (_) __ _  _ _   __ _ 
+ |  _// -_)| '_|(_-</ -_)/ _` || | |  _// _` || ' \  | |/ _` || ' \ / _` |
+ |_|  \___||_|  /__/\___|\__, ||_| |_|  \__,_||_||_|_/ |\__,_||_||_|\__, |
+                         |___/                     |__/             |___/ 
+""")
+
+# Input - Panjang & Lebar Persegi Panjang.
+while input_bangun_datar == "PERSEGI PANJANG":
+    try:
+        panjang_persegi_panjang = float(input("Masukkan panjang persegi panjang: "))
+        lebar_persegi_panjang = float(input("Masukkan lebar persegi panjang: "))
+
+        print("\n======= Luas Persegi Panjang =======")
+        print(f"Panjang Persegi Panjang: {panjang_persegi_panjang:,.2f} cm")
+        print(f"Lebar Persegi Panjang: {lebar_persegi_panjang:,.2f} cm")
+        print(f"Rumus Luas Persegi Panjang: {panjang_persegi_panjang:,.2f} cm x {lebar_persegi_panjang:.2f} cm")
+        print(f"Luas Persegi Panjang: {panjang_persegi_panjang * lebar_persegi_panjang:,.2f} cm²")
+
+        print("\n======= Keliling Persegi Panjang =======")
+        print(f"Panjang Persegi Panjang: {panjang_persegi_panjang:,.2f} cm")
+        print(f"Lebar Persegi Panjang: {lebar_persegi_panjang:,.2f} cm")
+        print(f"Rumus Keliling Persegi Panjang: 2 x ({panjang_persegi_panjang:,.2f} cm + {lebar_persegi_panjang:.2f} cm)")
+        print(f"Luas Persegi Panjang: {2 * (panjang_persegi_panjang + lebar_persegi_panjang):,.2f} cm")
+
+    except ValueError:
+        print("📢 Tolong Masukkan Angka!")
+        continue
+    else:
+        break
+
+if input_bangun_datar == "SEGITIGA":
+    print(r"""
+  ___             _  _    _             
+ / __| ___  __ _ (_)| |_ (_) __ _  __ _ 
+ \__ \/ -_)/ _` || ||  _|| |/ _` |/ _` |
+ |___/\___|\__, ||_| \__||_|\__, |\__,_|
+           |___/            |___/       
+""")
+
+# Input - Alas Segitiga.
+while input_bangun_datar == "SEGITIGA":
+    try:
+        alas_segitiga = float(input("Masukkan Alas Segitiga: "))
+    except ValueError:
+        print("📢 Tolong Masukkan Angka!")
+        continue
+    else:
+        break
+
+# Input - Tinggi Segitiga.
+while input_bangun_datar == "SEGITIGA":
+    try:
+        tinggi_segitiga = float(input("Masukkan Tinggi Segitiga: "))
+    except ValueError:
+        print("📢 Tolong Masukkan Angka!")
+        continue
+    else:
+        break
+
+# Input - Sisi Segitiga.
+while input_bangun_datar == "SEGITIGA":
+    try:
+        sisi_segitiga = float(input("Masukkan Sisi Segitiga: "))
+    except ValueError:
+        print("📢 Tolong Masukkan Angka!")
+        continue
+    else:
+        break
+
+# Output - Segitiga.
+if input_bangun_datar == "SEGITIGA":
+
+        print("\n======= Luas Segitiga =======")
+        print(f"Alas Segitiga: {alas_segitiga:,.2f} cm")
+        print(f"Tinggi Segitiga: {tinggi_segitiga:,.2f} cm")
+        print(f"Rumus Luas Segitiga: 1/2 x {alas_segitiga:,.2f} cm x {tinggi_segitiga:.2f} cm")
+        print(f"Luas Segitiga: {0.5 * alas_segitiga * tinggi_segitiga:,.2f} cm²")
+
+        print("\n======= Keliling Segitiga =======")
+        print(f"Sisi Segitiga: {sisi_segitiga:,.2f} cm")
+        print(f"Rumus Keliling Segitiga: {sisi_segitiga:,.2f} cm + {sisi_segitiga:.2f} cm + {sisi_segitiga:.2f} cm")
+        print(f"Keliling Segitiga: {sisi_segitiga * 3:,.2f} cm")
+
+if input_bangun_datar == "LINGKARAN":
+    print(r"""
+  _     _              _                          
+ | |   (_) _ _   __ _ | |__ __ _  _ _  __ _  _ _  
+ | |__ | || ' \ / _` || / // _` || '_|/ _` || ' \ 
+ |____||_||_||_|\__, ||_\_\\__,_||_|  \__,_||_||_|
+                |___/                             
+    """)
+
+# Input Jari-Jari Lingkaran.
+while True:
+    try:
+        jari_lingkaran = float(input("Masukkan Jari-Jari Lingkaran: "))
+
+        print("\n======= Luas Lingkaran =======")
+        print(f"Jari-Jari Lingkaran: {jari_lingkaran:,.2f} cm")
+        print(f"Rumus Luas Lingkaran: π x {jari_lingkaran:.2f} cm x {jari_lingkaran:.2f} cm")
+        print(f"Luas Lingkaran: {math.pi * jari_lingkaran ** 2:,.2f} cm²")
+
+        print("\n======= Keliling Lingkaran =======")
+        print(f"Rumus Keliling Lingkaran: 2 x π x {jari_lingkaran:.2f} cm")
+        print(f"Keliling Lingkaran: {2 * math.pi * jari_lingkaran:,.2f} cm")
+
+    except ValueError:
+        print("📢 Tolong Masukkan Angka!")
+        continue
+    else:
+        break
