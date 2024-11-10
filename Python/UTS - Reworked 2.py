365b35d692a6b5df0ebf07ac9aeea63e56c59b5d
Muhammad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-11-10 18:55:35 +0700

    🛠️ Maintenance - Small Change 2

--- Python/UTS - Reworked 2.py
+++ Python/UTS - Reworked 2.py
@@ -1,29 +1,36 @@
 import random
 
-print("Soal No 1 - CYMK")
-print("Soal No 2 - Mencari Bilangan")
+warna_campuran_1A = ["Warna Cyan + Warna Yellow = Green", "Warna Magenta + Warna Yellow = Red", "Warna Cyan + Warna Magenta = Blue"]
 
-pilih_soal = input("Pilih Soal (A/B): ").upper()
+print(r"""
+Soal No 1 - CMYK.
+        • A) Warna Otomatis
+        • B) Campuran 2 Warna
 
-# Soal Nomor 1A.
-if pilih_soal == "A":
-    print("Warna Cyan + Warna Yellow = Green")
+Soal No 2 - Mencari Urutan Bilangan.
+""")
 
-# Soal Nomor 1B.
-if pilih_soal == "B":
-    warna_1 = input("Masukkan Warna 1: ")
-    warna_2 = input("Masukkan Warna 2: ")
-    if warna_1 == ("Magenta" and warna_2 == "Yellow") or (warna_1 == "Yellow" and warna_2 == "Magenta"):
-        print("Red")
-    elif warna_1 == ("Cyan" and warna_2 == "Yellow") or (warna_1 == "Yellow" and warna_2 == "Cyan"):
-        print("Green")
-    elif warna_1 == ("Cyan" and warna_2 == "Magenta") or (warna_1 == "Magenta" and warna_2 == "Cyan"):
-        print("Blue")
+input_soal = input("Pilih Nomor Soal (1 / 2): ")
 
+if input_soal == "1":
+    input_soal_1 = input("Pilih Soal (A / B): ").upper()
+    if input_soal_1 == "A":
+        print(random.choice(warna_campuran_1A))
+
+    elif input_soal_1 == "B":
+        warna_1 = input("Masukkan Warna 1 (Magenta / Yellow / Cyan): ").capitalize()
+        warna_2 = input("Masukkan Warna 2 (Magenta / Yellow / Cyan): ").capitalize()
+        if warna_1 == ("Magenta" and warna_2 == "Yellow") or (warna_1 == "Yellow" and warna_2 == "Magenta"):
+            print("Red")
+        elif warna_1 == ("Cyan" and warna_2 == "Yellow") or (warna_1 == "Yellow" and warna_2 == "Cyan"):
+            print("Green")
+        elif warna_1 == ("Cyan" and warna_2 == "Magenta") or (warna_1 == "Magenta" and warna_2 == "Cyan"):
+            print("Blue")
+
+if input_soal == "2":
     bilangan_1 = int(input("Masukkan Bilangan 1: "))
     bilangan_2 = int(input("Masukkan Bilangan 2: "))
     bilangan_3 = int(input("Masukkan Bilangan 3: "))
-
     if bilangan_1 < bilangan_2 < bilangan_3:
         print("Urutan Naik")
     elif bilangan_1 > bilangan_2 > bilangan_3:
