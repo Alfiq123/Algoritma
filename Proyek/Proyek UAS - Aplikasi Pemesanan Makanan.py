e305d3ca0d0825fb83309c144381b50549ec8d14
Muhammad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-12-06 11:50:02 +0700

    🗂️ Project - UAS Bug Fixes 1.1

--- Proyek/Proyek UAS - Aplikasi Pemesanan Makanan.py
+++ Proyek/Proyek UAS - Aplikasi Pemesanan Makanan.py
@@ -58,7 +58,7 @@ def fungsi_utama():
 
     # Banner.
     print(r"""
-    ___        _                       ___           _                          _                _   
+     ___        _                       ___           _                          _                _   
     | _ \___ __| |_ ___ _ _ __ _ _ _   / __| __ _ _ _| |_ __ _ _ __  __ _ _ _   | |   ___ _____ _| |_ 
     |   / -_|_-<  _/ _ \ '_/ _` | ' \  \__ \/ _` | ' \  _/ _` | '_ \/ _` | ' \  | |__/ -_)_ / _` |  _|
     |_|_\___/__/\__\___/_| \__,_|_||_| |___/\__,_|_||_\__\__,_| .__/\__,_|_||_| |____\___/__\__,_|\__|
@@ -71,11 +71,7 @@ def fungsi_utama():
         print(f"Kategori: {kategori}")
 
         for menu, harga in menu_dan_harga.items():
-            # validasi_pesanan.append(menu)
             validasi_pesanan[menu] = harga
-
-            # placeholder[menu] = harga
-            
             print(f" • {menu:40} - Rp. {harga:,}")
 
         print()
