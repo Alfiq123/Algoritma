2be549569998babf12c764ef64798dc32cf4e67a
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-04-23 21:29:55 +0700

    🎓 Study 11 - Changes

--- Algoritma - Tugas/Pekerjaan/Testing.py
+++
@@ -1,8 +0,0 @@
-# Show only the first few and last few digits
-def summarize_large_number(n):
-    s = str(n)
-    if len(s) > 20:
-        return f"{s[:10]}...{s[-10:]} (length: {len(s)})"
-    return s
-
-print(summarize_large_number(123456789012345678901234567890))
