77999116a8ea7dc2ce869923e4b200ebe0a741d6
Alfiq123 <fauzitaufiqurrahman218@gmail.com>
2025-04-29 16:30:38 +0700

    🎓 Study 12 - Changes 3

--- Algoritma - Latihan/Python Semester 2/Apr 24 - Rekursi Deret Berpola.py
+++ Algoritma - Latihan/Python Semester 2/Apr 24 - Rekursi Deret Berpola.py
@@ -4,21 +4,21 @@ def deret1(n):
         return [3]
     else:
         return deret1(n - 1) + [(-1) ** (n + 1) * 3 * n]
-    
+
 def deret2(n):
     """ S = 1 - 3 + 5 - 7 + 9 - 11 + 13 - ... """
     if n == 1:
         return [1]
     else:
         return deret2(n - 1) + [round((-1) ** (n - 1) * (2 * n - 1), 4)]
-    
+
 def deret3(n):
     """ S = (1/1) - (1/2) + (1/3) - (1/4) + (1/5) - ... """
     if n == 1:
         return [1]
     else:
         return deret3(n - 1) + [round((-1) ** (n + 1) * (1 / n), 4)]
-    
+
 def deret4(n):
     """ S = (1/2) - (1/4) + (1/6) - (1/8) + (1/10) - ... """
     if n == 1:
@@ -33,21 +33,21 @@ def deret5(n):
     else:
         return deret5(n - 1) + [round((-1) ** (n + 1) * (2 ** n) / (3 ** n), 4)]
 
-print(deret1(10))
-print(deret2(10))
-print(deret3(10))
-print(deret4(10))
-print(deret5(10))
+print(
+    f"{deret1(10)}\n"
+    f"{deret2(10)}\n"
+    f"{deret3(10)}\n"
+    f"{deret4(10)}\n"
+    f"{deret5(10)}\n"
+)
 
-print()
-
-print(round(sum(deret1(10)), 4))
-print(round(sum(deret2(10)), 4))
-print(round(sum(deret3(10)), 4))
-print(round(sum(deret4(10)), 4))
-print(round(sum(deret5(10)), 4))
-
-print()
+print(
+    f"The Sum of 1: {sum(deret1(10))}\n"
+    f"The Sum of 2: {sum(deret2(10))}\n"
+    f"The Sum of 3: {round(sum(deret3(10)), 4)}\n"
+    f"The Sum of 4: {round(sum(deret4(10)), 4)}\n"
+    f"The Sum of 5: {round(sum(deret5(10)), 4)}\n"
+)
 
 def deret_gpt1(n):
     """ S = 5 - 10 + 15 - 20 + 25 - 30 + 35 - ... """
@@ -62,7 +62,7 @@ def deret_gpt2(n):
         return [2]
     else:
         return deret_gpt2(n - 1) + [(-1) ** (n + 1) * 2 * n]
-    
+
 def deret_gpt3(n):
     """ S = 1/1 - 2/2 + 3/3 - 4/4 + 5/5 - 6/6 + ... """
     if n == 1:
@@ -76,7 +76,7 @@ def deret_gpt4(n):
         return [round(1 / 3), 4]
     else:
         return deret_gpt4(n - 1) + [round((-1) ** (n + 1) * (1 / (3 * n)), 4)]
-    
+
 def deret_gpt5(n):
     """ S = 3/2 - 9/4 + 27/8 - 81/16 + ... """
     if n == 1:
@@ -84,18 +84,18 @@ def deret_gpt5(n):
     else:
         return deret_gpt5(n - 1) + [round((-1) ** (n + 1) * (3 ** n) / (2 ** n), 4)]
 
-print(deret_gpt1(10))
-print(deret_gpt2(10))
-print(deret_gpt3(10))
-print(deret_gpt4(10))
-print(deret_gpt5(10))
+print(
+    f"{deret_gpt1(10)}\n"
+    f"{deret_gpt2(10)}\n"
+    f"{deret_gpt3(10)}\n"
+    f"{deret_gpt4(10)}\n"
+    f"{deret_gpt5(10)}\n"
+)
 
-print()
-
-print(sum(deret_gpt1(10)))
-print(sum(deret_gpt2(10)))
-print(sum(deret_gpt3(10)))
-print(sum(deret_gpt4(10)))
-print(sum(deret_gpt5(10)))
-
-print()
+print(
+    f"The Sum of 1: {sum(deret_gpt1(10))}\n"
+    f"The Sum of 2: {sum(deret_gpt2(10))}\n"
+    f"The Sum of 3: {sum(deret_gpt3(10))}\n"
+    f"The Sum of 4: {round(sum(deret_gpt4(10)), 4)}\n"
+    f"The Sum of 5: {round(sum(deret_gpt5(10)), 4)}\n"
+)
